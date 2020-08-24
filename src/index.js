e5d5a5f3ab26e1053b0c340038c6abfca0ee7975
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-08-24 15:33:55 +0500

    Добавит разметку компонентов

--- src/index.js
+++ src/index.js
@@ -1,39 +1,6 @@
-class SwapiService {
-  _apiBase = `https://swapi.co/api`;  
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app.jsx';
 
-  async getResource(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
-    
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}` + `, received ${res.status}`);  
-    }
-    return await res.json();
-  } 
-  
-  getAllPeople() {
-    const res = this.getResource(`/people/`);  
-    return res.results;
-  }
-
-  getPerson(id) {
-    return this.getResource(`/people/${id}/`);  
-  }
-
-  getAllPlanets() {
-    const res = this.getResource(`/planets/`);  
-    return res.results;
-  }
-
-  getPlanet(id) {
-    return this.getResource(`/planets/${id}/`);  
-  }
-
-  getAllStarships() {
-    const res = this.getResource(`/starships/`);  
-    return res.results;
-  }
-
-  getStarship(id) {
-    return this.getResource(`/starships/${id}/`);  
-  }
-}
+ReactDOM.render(<App />,
+  document.getElementById('root'));
