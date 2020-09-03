ba22b690a0b503f3ab43b40845d30a1e5db0757d
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-09-03 11:04:48 +0500

    Добавит в ItemList ComponentDidMount

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Header from '../header/header.jsx';
 import RandomPlanet from '../random-planet/random-planet.jsx';
 import ItemList from '../item-list/item-list.jsx';
@@ -7,22 +6,56 @@ import PersonDetails from '../person-details/person-details.jsx';
 
 import './app.css';
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
+class App extends React.Component {
+  constructor() {
+    super();
 
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
+    this.state = {
+      showRandomPlanet: true,
+      selectedPerson: null
+    };
+  
+  }
+
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      }
+    });
+  };
+
+  onPersonSelected = (id) => {
+    this.setState({
+      selectedPerson: id
+    });
+  };
+
+  render() {
+    const planet = this.state.showRandomPlanet ? <RandomPlanet/> : null;
+
+    return (
+      <div className="stardb-app">
+        <Header />
+        {planet}
+
+        <button
+          className="toggle-planet btn btn-warning btn-lg"
+          onClick={this.toggleRandomPlanet}>
+          Toggle Random Planet
+        </button>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected}/>
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
